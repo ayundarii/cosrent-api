494495681339cf35efb43c10e2cfaae6d84c69ae
ayundarii <ayu13pradnyandari@gmail.com>
2023-11-13 15:12:42 +0800

    ⭐ Create CRUD for Category Controller

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -3,8 +3,76 @@ const prisma = new PrismaClient()
 
 class CategoryController {
     static async getCategory(req, res){
-        const result = prisma.category.findMany()
+        const result = await prisma.category.findMany()
+        
         res.json({ data: result })
+    }
+
+    static async getCategoryById(req, res){
+        const result = await prisma.category.findUnique({
+            where: {
+                id: Number (req.params.id)
+            }
+        })
+    
+        if(result){
+            res.status(200).json({ data: result })  
+        } else {
+            res.status(404).json({ message: "Data not found" })
+        }
+    }
+
+    static async addCategory(req, res){
+        const result = await prisma.category.create({
+            data: {
+                category: req.body.category,
+                description: req.body.description
+            }
+        })
+    
+        res.status(201).json({ data: result, message: "Data Input Success" })
+    }
+
+    static async deleteCategory(req, res){
+        try {
+            const result = await prisma.category.findUnique({
+                where: {
+                    id: Number (req.params.id)
+                }
+            })
+          
+            await prisma.category.delete({
+                where: {
+                    id: Number (req.params.id)
+                }
+            })
+
+            res.status(200).json({ message: 'Category succesfully deleted.'})
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError && error.code === 'P2025'){
+                return res.status(404).json({ error: 'Category ID does not exist' })
+            }
+
+            res.status(500).json({ error: 'An error occured while trying to delete a category' })
+        }
+    }
+
+    static async updateCategory(req, res){
+        const update = await prisma.category.update({
+            where: {
+                id: Number (req.params.id)
+            },
+            data: {
+                category: req.body.genre,
+                description: req.body.description
+            }
+        })
+
+        if(update){
+            res.status(201).json({ data: update, message: "Data succesfully updated" })
+        } else {
+            res.status(404).json({ message: "Data not found" })
+        }
     }
 }
 
--- routers/category.router.js
+++ routers/category.router.js
@@ -4,5 +4,9 @@ const CategoryController = require("../controllers/category.controller")
 const router = Router()
 
 router.get("/categories", CategoryController.getCategory)
+router.get("/categories/:id", CategoryController.getCategoryById)
+router.post("/categories/add", CategoryController.addCategory)
+router.delete("/categories/:id", CategoryController.deleteCategory)
+router.put("/categories/:id/update", CategoryController.updateCategory)
 
 module.exports = router
